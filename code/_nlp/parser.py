5ceda2d0395d59b405f56987526fc88b0c2dea03
Sav <dsouvik141@gmail.com>
2025-09-25 18:28:25 +0530

    Initial commit

--- code/_nlp/parser.py
+++ code/_nlp/parser.py
@@ -3,7 +3,8 @@ import joblib
 import spacy
 
 nlp = spacy.load("en_core_web_md")  # use vectors
-clf = joblib.load("category_svm.pkl")
+category_clf = joblib.load("category_svm.pkl") # category classifier
+txn_type_clf = joblib.load("txn_type_svm.pkl") # category classifier
 
 def parse(text: str):
     doc = nlp(text)
@@ -12,17 +13,6 @@ def parse(text: str):
     amount_match = re.search(r"\d+(?:\.\d{1,2})?", text)
     amount = float(amount_match.group()) if amount_match else None
 
-    # Intent classification
-    lower_text = text.lower()
-    if "spent" in lower_text or "bought" in lower_text:
-        tx_type = "Expense"
-    elif "sent" in lower_text or "transfer" in lower_text:
-        tx_type = "Transfer"
-    elif "gave" in lower_text or "lend" in lower_text:
-        tx_type = "Lend"
-    else:
-        tx_type = "Other"
-
     # Extract entities
     entities = [{"text": ent.text, "label": ent.label_} for ent in doc.ents]
 
@@ -30,11 +20,12 @@ def parse(text: str):
     category = None
     
     vector = doc.vector.reshape(1, -1)
-    category = clf.predict(vector)[0]
+    category = category_clf.predict(vector)[0]
+    type = txn_type_clf.predict(vector)[0]
 
     return {
-        "transaction_type": tx_type,
         "amount": amount,
         "entities": entities,
-        "category": category
+        "category": category,
+        "transaction_type": type
     }
--- code/_nlp/train.py
+++ code/_nlp/train.py
@@ -7,25 +7,38 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import joblib
 
-# Load SpaCy model with vectors (use en_core_web_md or en_core_web_lg, not sm)
-nlp = spacy.load("en_core_web_md")
+def train():
+    # Load SpaCy model with vectors (use en_core_web_md or en_core_web_lg, not sm)
+    nlp = spacy.load("en_core_web_md")
 
-training_data_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdDuM9UfluHPKV_ja9cAH6--7wFnMeSwKi7W32n_MQ63KD1abuymGs6FUM5EnTo-X7WyXlnTIMbEFS/pub?output=csv"
-df = pd.read_csv(training_data_csv)
+    training_data_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdDuM9UfluHPKV_ja9cAH6--7wFnMeSwKi7W32n_MQ63KD1abuymGs6FUM5EnTo-X7WyXlnTIMbEFS/pub?output=csv"
+    df = pd.read_csv(training_data_csv)
 
-# Convert texts into vectors
-X = np.array([nlp(text).vector for text in df.iloc[:, 1].astype(str)])
-y = df.iloc[:, 0].values
+    # Convert texts into vectors
+    X = np.array([nlp(text).vector for text in df.iloc[:, 1].astype(str)]) # texts
+    y = df.iloc[:, 0].values # labels
 
-# Split & train SVM
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-clf = svm.SVC(kernel="linear", probability=True)
-clf.fit(X_train, y_train)
+    # Split & train SVM for Category Identification
+    X_train, X_test, y_train, labels_y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    clf = svm.SVC(kernel="linear", probability=True)
+    clf.fit(X_train, y_train)
 
-# Evaluate
-y_pred = clf.predict(X_test)
+    # Evaluate
+    labels_y_pred = clf.predict(X_test)
 
-print(classification_report(y_test, y_pred))
+    # Save the trained model
+    joblib.dump(clf, "category_svm.pkl")
 
-# Save the trained model
-joblib.dump(clf, "category_svm.pkl")
+    y = df.iloc[:, 2].values # types
+    # Split & train SVM for Category Identification
+    X_train, X_test, y_train, types_y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    clf = svm.SVC(kernel="linear", probability=True)
+    clf.fit(X_train, y_train)
+
+    # Evaluate
+    types_y_pred = clf.predict(X_test)
+
+    # Save the trained model
+    joblib.dump(clf, "txn_type_svm.pkl")
+
+    return f"{classification_report(labels_y_test, labels_y_pred)}\n{classification_report(types_y_test, types_y_pred)}"
